0a190b3b64f59622ccc20fe748d5093c733d91ae
Juan Jose Gutierrez Ortega <juangutierrezortega@MacBook-Pro-de-Juan.local>
2022-10-09 12:02:30 +0200

    feat: add loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 import { ProvideCards, useCards } from "./domains/cards/context";
 import { getMyCards } from "./domains/cards/storage";
 import { initStorage } from "./lib/storage";
@@ -7,15 +8,31 @@ import LandingScreen from "./screens/Landing";
 initStorage(localStorage);
 
 const Screens = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const { setCards } = useCards();
 
   useEffect(() => {
     const loadMyCards = async () => {
       const myCards = await getMyCards();
-      setCards(myCards || [])
+      setCards(myCards || []);
+      // he añadido este timeout para simular una carga inicial
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 2000)
     };
     loadMyCards();
-  }, [])
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  }
 
   return <LandingScreen />;
 };
